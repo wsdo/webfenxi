d51cfcbcec2bb2f87abae0a35007061a5c1d651e
wshudong <wsd312@163.com>
2016-07-13 21:34:40 +0800

    add pastdue

--- modules/v1/controllers/TestController.php
+++ modules/v1/controllers/TestController.php
@@ -2,17 +2,21 @@
 
 namespace app\modules\v1\controllers;
 
-use app\services\V8jsService;
+// use app\services\V8jsService;
 use yii\rest\ActiveController;
 
 class TestController extends \yii\web\Controller
 {
-    public $bar = null;
+    private $v8 = false;
+
     public function actionIndex()
     {
-        $v8 = new V8jsService();
+        phpinfo();
+        // $this->v8 = new V8Js();
+        // $this->v8->executeString($js);
+        // $v8 = new V8jsService();
         // This prints "no"
-        $v8->executeString('print( "bar" in PHP.foo ? "yes" : "no" );');
+        $this->v8->executeString('print( "bar" in PHP.foo ? "yes" : "no" );');
 
         // return $this->render('index');
     }
